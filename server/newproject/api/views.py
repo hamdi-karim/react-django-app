c0969ab0f22fb3fe6ad764e63e042144b1fabc82
hamdi-karim <k.hamdi1995@gmail.com>
2024-10-16 16:54:21 +0100

    prepare getting all books endpoint

--- server/newproject/api/views.py
+++ server/newproject/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializer import BookSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_books(request):
+    booksData = Book.objects.all()
+    serializedData = BookSerializer(booksData, many=True)
+    return Response(serializedData)
