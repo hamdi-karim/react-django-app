3b6d0fbd5e9ba373a49b0f7322d85be730ef8b76
hamdi-karim <k.hamdi1995@gmail.com>
2024-10-16 11:22:54 +0100

    setup project

---
+++ client/app/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1> Book Storm </h1>
+
+      <div>
+        <input type="text" placeholder="Title" />
+        <input type="number" placeholder="Release date" />
+        <button>Add Book</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
