611387d301eaeadb3b80a2ed7eefaaf513baf4fb
hamdi-karim <k.hamdi1995@gmail.com>
2024-10-17 12:52:04 +0100

    get and display books list from the backend in React

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,6 +1,25 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
+      const data = await response.json();
+
+      setBooks(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
   return (
     <>
       <h1> Book Storm </h1>
@@ -9,6 +28,14 @@ function App() {
         <input type="text" placeholder="Title" />
         <input type="number" placeholder="Release date" />
         <button>Add Book</button>
+
+        <h1>Books List</h1>
+        {books.map((book) => (
+          <div key={book.id}>
+            <p>Title: {book.title} </p>
+            <p>Release Year: {book.release_year}</p>
+          </div>
+        ))}
       </div>
     </>
   );
