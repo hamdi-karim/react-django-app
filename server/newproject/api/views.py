c79136aa060a1ca3660c6a6a4986ef9ce935961c
hamdi-karim <k.hamdi1995@gmail.com>
2024-10-17 11:15:46 +0100

    add new endpoint for creating a new Book entry

--- server/newproject/api/views.py
+++ server/newproject/api/views.py
@@ -9,3 +9,12 @@ def get_books(request):
     booksData = Book.objects.all()
     serializedData = BookSerializer(booksData, many=True).data
     return Response(serializedData)
+
+@api_view(['POST'])
+def create_book(request):
+    data = request.data
+    serializer = BookSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
